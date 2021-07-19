7493213924e3fab97dd6e8b30f93823130286673
Gwak seyeon <ksyk1205@naver.com>
2021-07-19 20:58:40 +0900

    객체지향 람다식 비교

--- src/ch05/PrintStringTest.java
+++ src/ch05/PrintStringTest.java
@@ -5,8 +5,18 @@ public class PrintStringTest {
 	public static void main(String[] args) {
 		PrintString lambdaStr = s->System.out.println(s);  //람다식을 변수에 대입
 		lambdaStr.showString("hello lambda_1");
-
+		
+		showMyString(lambdaStr); 
 
 	}
+	
+	public static void showMyString(PrintString p) {
+		p.showString("hello lambda_2");
+	}
+	
+	
+
+
+
 
 }
--- src/ch05/showMyString.java
+++ src/ch05/showMyString.java
@@ -1,5 +1,6 @@
 package ch05;
 
 public class showMyString {
+	
 
 }
